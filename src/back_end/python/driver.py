3198f6392609be44bc272bbe12dbc923200944fd
Robert Clarke <clarke.john.robert@gmail.com>
2019-09-13 11:54:23 -0400

    Started Service wrapper interface

---
+++ src/back_end/python/driver.py
@@ -0,0 +1,37 @@
+
+
+
+
+class MusicWrapper(object):
+    """
+    Abstract class for a MusicWrapper object
+    Declares the interface for a streaming service wrapper
+    """
+    def __init__(self):
+        pass
+
+    def start(self, link: str, start_time = 0):
+        raise NotImplementedError
+
+    def play(self):
+        """Unpauses the wrapped video"""
+        raise NotImplementedError
+
+    def pause(self):
+        """Pauses the wrapped video"""
+        raise NotImplementedError
+
+    def goto(self, timestamp):
+        """Set the song to specified time"""
+        raise NotImplementedError
+
+    def jump(self, direction: str, timeamount):
+        """Jump forward or back the specified number of seconds"""
+        raise NotImplementedError
+
+    def skip(self):
+        """Skips to the end of the wrapped song"""
+        raise NotImplementedError
+
+    def speed(self, speed_value: float = 1.0):
+        raise NotImplementedError
